aa67a2c68a032afdf2a4c32fe3052335c7672c00
dev.diwakar <diwakarwork4586@gmail.com>
2026-03-08 12:05:09 +0530

    configuration updates for build

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { enviorment } from '../../../enviorment/enviorment';
+import { enviorment } from '../../../enviorments/environment';
 
 @Injectable({
     providedIn: 'root',
