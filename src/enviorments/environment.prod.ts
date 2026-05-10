20602368971e5fce8971d8bf1ddac2efdbccfd19
diwakar.dev <diwakarwork4586@gmail.com>
2026-05-10 14:44:49 +0530

    fix : base url update

--- src/enviorments/environment.prod.ts
+++ src/enviorments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://test-management-apis-production.up.railway.app/api',
+    // baseUrl: 'https://test-management-apis-production.up.railway.app/api',
+    baseUrl: 'https://test-management-apis.onrender.com/api',
     googleClientId:'713000365254-jo55s5h614h6g4f5vr3rpob3atvi9vb9.apps.googleusercontent.com'
-
 }
